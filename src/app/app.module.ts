633040d534036b94c93eaab151fd31f710fa06fb
Sushma <sushmapreddy@gmail.com>
2024-10-27 19:47:00 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,4 @@
-// import { HttpClientModule } from '@angular/common/http';
-// import { NgModule } from '@angular/core';
-// import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-// import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-// import { AppComponent } from './app.component';
-
-// import { ApolloModule } from 'apollo-angular';
-// import {OrderComponent} from "./components/order/order.component";
-// import {CommonModule} from "@angular/common";
-// import {AngularGraphQLModule, GraphQLModule} from "./graphql.module";
-
-// @NgModule({
-//   declarations: [
-//     AppComponent,
-//     // AddTutorialComponent,
-//     // TutorialDetailsComponent,
-//     // TutorialsListComponent,
-//     OrderComponent,
-//   ],
-//   imports: [
-//     BrowserModule,
-//     CommonModule,
-//     ReactiveFormsModule,
-//     AppRoutingModule,
-//     FormsModule,
-//     HttpClientModule,
-//     ApolloModule,
-//     AngularGraphQLModule
-//   ],
-//   providers: [],
-//   bootstrap: [AppComponent],
-// })
-// export class AppModule {}
-// import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
-
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
@@ -45,7 +9,7 @@ import { AppComponent } from './app.component';
 import {GraphQLModule} from "./graphql.module";
 import {EventComponent} from "./components/event/event.component";
 import {PublishEventComponent} from "./components/publish-event/publish-event.component";
-import {RouterOutlet} from "../../node_modules-gws/@angular/router";
+import { RouterOutlet } from '@angular/router';
 
 @NgModule({
   declarations: [
